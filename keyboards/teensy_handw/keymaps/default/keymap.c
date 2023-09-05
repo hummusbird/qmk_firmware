48b83106c459fef1d1e105ca025baec5e0195823
hummusbird <birb@birb.cc>
2023-09-05 21:31:00 +0100

    define

--- keyboards/teensy_handw/keymaps/default/keymap.c
+++ keyboards/teensy_handw/keymaps/default/keymap.c
@@ -23,21 +23,6 @@ bool oled_task_user(void) {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        case _FN:
-            oled_write_P(PSTR("FN\n"), false);
-            break;
-        case _ADJ:
-            oled_write_P(PSTR("ADJ\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
@@ -47,4 +32,3 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
-
