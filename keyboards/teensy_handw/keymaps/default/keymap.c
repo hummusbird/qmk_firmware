af7b85c3001e200babca08192f3bf9b409053db0
hummusbird <birb@birb.cc>
2023-09-06 09:56:59 +0100

    oled display

--- keyboards/teensy_handw/keymaps/default/keymap.c
+++ keyboards/teensy_handw/keymaps/default/keymap.c
@@ -19,15 +19,17 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
 #ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_90;
+}
 
+bool oled_task_user(void) {
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("caps ") : PSTR("     "), false);
+    oled_write_P(led_state.num_lock ? PSTR("numlk\n") : PSTR("     \n"), false);
+
+    oled_write_ln_P(PSTR("----------"), false);
 
     return false;
 }
