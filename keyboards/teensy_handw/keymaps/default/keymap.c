01a58b030331892b5641e3e19c5014dd882ed908
hummusbird <birb@birb.cc>
2023-09-07 11:02:12 +0100

    add cycling line

--- keyboards/teensy_handw/keymaps/default/keymap.c
+++ keyboards/teensy_handw/keymaps/default/keymap.c
@@ -10,7 +10,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCROLL_LOCK, KC_PAUS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_HASH, KC_LSFT, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LALT, KC_LGUI, KC_MENU, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT)};
 
-
+#define CYCLE_DURATION 6000
+uint32_t timer = 0;
+uint8_t current_cycle = 0;
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -43,14 +45,58 @@ static void draw_os(void) {
     }
 }
 
+static void draw_l_last_char(bool on) {
+    oled_write_pixel(60, 126, on);
+    oled_write_pixel(61, 126, on);
+    oled_write_pixel(62, 126, on);
+
+    oled_write_pixel(61, 125, on);
+    oled_write_pixel(61, 124, on);
+    oled_write_pixel(61, 123, on);
+    oled_write_pixel(61, 122, on);
+    oled_write_pixel(61, 121, on);
+
+    oled_write_pixel(60, 120, on);
+    oled_write_pixel(61, 120, on);
+}
+
+static void draw_cycle(void) {
+    if (timer_elapsed(timer) > CYCLE_DURATION) {
+        timer = timer_read();
+        current_cycle++;
+        if (current_cycle >= 3) {
+            current_cycle = 0;
+        }
+    }
+
+    oled_set_cursor(0, 15); // set cursor to last line
+
+    switch(current_cycle) {
+        case 0:
+            draw_os();
+            break;
+        case 1:
+            oled_write_P(PSTR("birb.cc/tk"), false);
+            draw_l_last_char(true);
+            break;
+        case 2:
+            oled_write_P(PSTR("> v0.0 tkl"), false);
+            draw_l_last_char(false);
+            break;
+    }
+}
+
 bool oled_task_user(void) {
-    // Host Keyboard LED Status
+    oled_set_cursor(0, 0);
     led_t led_state = host_keyboard_led_state();
+
     oled_write_ln_P(led_state.caps_lock ? PSTR(" capslock") : PSTR(""), false);
-
     oled_write_P(PSTR("+--------+"), false);
+    oled_write_ln_P(led_state.num_lock ? PSTR(" num lock") : PSTR(""), false);
 
-    draw_os();
+    oled_write_ln_P(PSTR("wpm: "), false);
+
+    draw_cycle();
 
     return false;
 }
