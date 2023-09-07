2d21c793964bf66debfd1b7c665e384b50052d20
hummusbird <birb@birb.cc>
2023-09-07 10:11:26 +0100

    fix OS detect

--- keyboards/teensy_handw/keymaps/default/keymap.c
+++ keyboards/teensy_handw/keymaps/default/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "os_detection.h"
+#include "string.h"
 
 /* THIS FILE WAS GENERATED!
  *
@@ -8,6 +9,8 @@
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCROLL_LOCK, KC_PAUS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_HASH, KC_LSFT, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LALT, KC_LGUI, KC_MENU, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT)};
+
+
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -20,26 +23,34 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;
 }
 
+static void draw_os(void) {
+    switch (detected_host_os()) {
+        case OS_UNSURE:
+            oled_write_ln_P(PSTR(""), false);
+            break;
+        case OS_LINUX:
+            oled_write_ln_P(PSTR("> Linux"), false);
+            break;
+        case OS_WINDOWS:
+            oled_write_ln_P(PSTR("> Windows"), false);
+            break;
+        case OS_MACOS:
+            oled_write_ln_P(PSTR("> OS X"), false);
+            break;
+        case OS_IOS:
+            oled_write_ln_P(PSTR("> iOS"), false);
+            break;
+    }
+}
+
 bool oled_task_user(void) {
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.caps_lock ? PSTR("caps ") : PSTR("     "), false);
-    oled_write_P(led_state.num_lock ? PSTR("numlk\n") : PSTR("     \n"), false);
+    oled_write_ln_P(led_state.caps_lock ? PSTR(" capslock") : PSTR(""), false);
 
-    oled_write_ln_P(PSTR("+--------+"), false);
+    oled_write_P(PSTR("+--------+"), false);
 
-    switch (detected_host_os()) {
-        case OS_UNSURE:
-            oled_write_P(PSTR("> Unknown!"), false);
-        case OS_LINUX:
-            oled_write_P(PSTR("> Linux   "), false);
-        case OS_WINDOWS:
-            oled_write_P(PSTR("> Windows "), false);
-        case OS_MACOS:
-            oled_write_P(PSTR("> OS X    "), false);
-        case OS_IOS:
-            oled_write_P(PSTR("> iOS     "), false);
-    }
+    draw_os();
 
     return false;
 }
