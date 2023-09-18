eaaa95cb5b0d59c0a80589b1b6ac7a44038cb2db
hummusbird <birb@birb.cc>
2023-09-18 13:17:31 +0100

    fix keymap

--- keyboards/teensy_handw/keymaps/default/keymap.c
+++ keyboards/teensy_handw/keymaps/default/keymap.c
@@ -9,7 +9,7 @@
  * edit it directly.
  */
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCROLL_LOCK, KC_PAUS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_HASH, KC_LSFT, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LALT, KC_LGUI, KC_MENU, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCROLL_LOCK, KC_PAUS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_LSFT, KC_NONUS_BACKSLASH, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LALT, KC_LGUI, KC_MENU, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT)};
 
 #define CYCLE_DURATION 6000
 uint32_t timer = 0;
@@ -72,7 +72,7 @@ static void draw_cycle(void) {
     if (timer_elapsed(timer) > CYCLE_DURATION) {
         timer = timer_read();
         current_cycle++;
-        if (current_cycle >= 3) {
+        if (current_cycle >= 4) {
             current_cycle = 0;
         }
     }
@@ -88,17 +88,25 @@ static void draw_cycle(void) {
             draw_l_last_char(true);
             break;
         case 2:
-            oled_write_P(PSTR("> v0.0 tkl"), false);
+            oled_write_P(PSTR("> v0.1 tkl"), false);
             draw_l_last_char(false);
             break;
+        case 3:
+            oled_write_P(PSTR("> MT3 2048"), false);
     }
 }
 
 static void draw_wpm(void) {
-    oled_write_ln_P(PSTR("wpm: "), false);
+    oled_write_P(PSTR("wpm: "), false);
     char str[5];
     sprintf(str, "%d", get_current_wpm());
     oled_write_ln(str, false);
+}
+
+static void draw_keycount(void) {
+    oled_write_ln_P(PSTR(""), false);
+    oled_write_ln_P(PSTR("count:"), false);
+    oled_write_ln("0123456789", false);
 }
 
 bool oled_task_user(void) {
@@ -106,6 +114,7 @@ bool oled_task_user(void) {
 
     draw_leds();
     draw_wpm();
+    draw_keycount();
     draw_cycle();
 
     return false;
